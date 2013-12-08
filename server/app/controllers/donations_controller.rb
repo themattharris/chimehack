58a4a7d35c7ba9075d042af968b725ad7c0aa0eb
mdp <mdp@yahoo-inc.com>
2013-12-07 16:23:30 -0800

    Fix the new donation page

--- server/app/controllers/donations_controller.rb
+++ server/app/controllers/donations_controller.rb
@@ -6,8 +6,14 @@ class DonationsController < ApplicationController
   def thanks
   end
 
+  def new
+    @user = User.find(1) # Hardcode to Allison
+    @challenge = Challenge.find(1)
+  end
+
   def create
     @user = User.find(1) # Hardcode to Allison
+    @challenge = Challenge.find(1)
     @donor = User.create({:name => params[:cardholder], :email => params[:email] })
     Donation.create({:referrer_id => @user.id, :donor => @donor, :value => params[:amount]})
     redirect_to("/donations/thanks")
