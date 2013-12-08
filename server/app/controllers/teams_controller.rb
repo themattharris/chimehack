872ca3cc8a2da1238ac3db7b969906f591db7043
themattharris <mharris@twitter.com>
2013-12-07 16:27:47 -0800

    handle no donor

--- server/app/controllers/teams_controller.rb
+++ server/app/controllers/teams_controller.rb
@@ -4,7 +4,13 @@ class TeamsController < ApplicationController
     @team = Team.find(1)
     @challenge = Challenge.find(1)
 
-    @donations = Donation.where(challenge_id: @challenge.id).sort_by {|donation| donation.donor.total_donations}.reverse
+    @donations = Donation.where(challenge_id: @challenge.id).sort_by do |donation|
+      if donation.donor.present?
+        donation.donor.total_donations
+      else
+        0
+      end
+    end.reverse
     @donations = @donations[0..2]
 
     @user = @team.users.first # Shoud be Allison
